d94a8e8abe17c16599546aa82c860d00a34de821
claraid15 <claraindia15@gmail.com>
2026-05-31 17:36:21 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,9 @@
-from .pipelines.embedding import VAEPipeline, AEPipeline
-from .pipelines.clustering import LeidenClusteringPipeline
-
-from src.clustering import LeidenClustering
-
+from src.pipelines.embedding import VaeClusteringPipeline
 
 if __name__ == "__main__":
-    # vae_pipeline = VAEPipeline(
-    #     config_path="vae_embedding.yml"
-    # )
-    ae_pipeline = AEPipeline(
-        config_path="ae_embedding.yml"
+    pipeline = VaeClusteringPipeline(
+        config_path="config/embedding.yml",
+        preprocessing_config_path="config/preprocessing.yml",
     )
 
-    # vae_pipeline.run()
-    ae_pipeline.run()
+    pipeline.run()
